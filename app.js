b606c27a44cfd755f2a54af63576607c28b6d936
techstackmedia <osagienoah04@gmail.com>
2021-12-12 13:23:44 +0100

    config folder to store the mongo connection string in config/keys.js - ignored

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const app = express();
+const db = require("./config/keys").MongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 const port = process.env.PORT || 3000;
 
 app.use("/", require("./routes/index"));
