ec7581be561be36a585359e25790bfbedec0944e
techstackmedia <osagienoah04@gmail.com>
2021-12-12 12:56:56 +0100

    add routes and basic layout to mount other view

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -6,6 +8,10 @@ app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 app.use("/login", require("./routes/users"));
 
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
 app.listen(port, () => {
-  console.log(`App listening on ${port}`);
+  console.log(`App running on https://localhost:${port}`);
 });
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/login", (req, res) => {
-  res.send("Login");
+  res.render("login");
 });
 
 router.get("/register", (req, res) => {
-  res.send("Register");
+  res.render("register");
 });
 
 module.exports = router;
